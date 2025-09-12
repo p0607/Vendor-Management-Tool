74f2c54a2fa36471722e9c3b7d3c7a661f07a90f
Priyanka Sahani <priyanka1sahani1@gmail.com>
2025-09-13 01:50:25 +0530

    SQL INSERT STATEMENT FIXED

--- Vendor_Management_BackEnd/index.js
+++ Vendor_Management_BackEnd/index.js
@@ -926,7 +926,7 @@ app.post('/api/team-report', async (req, res, next) => {
           tower, client_name, project_name, business_unit, bu_head, hc,
           salary_cost, sales, gpm, gpm_percentage, leave_encashment,
           team_cost, opr_cost, funding_cost, np, np_percentage, month, year
-        ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19) RETURNING *`,
+        ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18) RETURNING *`,
         [
           tower === '' ? null : tower,
           client_name === '' ? null : client_name,
@@ -1025,7 +1025,7 @@ app.post('/api/team-report/bulk', async (req, res, next) => {
         tower, client_name, project_name, business_unit, bu_head, hc,
         salary_cost, sales, gpm, gpm_percentage, leave_encashment,
         team_cost, opr_cost, funding_cost, np, np_percentage, month, year
-      ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19)`;
+      ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18)`;
       
       for (let i = 0; i < data.length; i++) {
         const record = data[i];
